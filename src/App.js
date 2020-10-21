2a913819658b4861bf015b4216992f1244a0e29d
Dillon Hayes <G00373320@gmit.ie>
2020-10-21 13:21:34 +0100

    Lab Complete

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
-import { Content } from './components/contents';
 import { Navbar, Nav } from 'react-bootstrap';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Create } from './components/create';
+import { Read } from './components/read';
+import { Content } from './components/content';
 
 
 class App extends Component {
@@ -28,8 +30,8 @@ class App extends Component {
           <br />
           <switch>
             <Route path='/' Component={Content} exact />
-            <Route path='/create' component={Header} exact />
-            <Route path='/read' component={Footer} exact />
+            <Route path='/create' component={Create} exact />
+            <Route path='/read' component={Read} exact />
           </switch>
         </div>
       </Router>
